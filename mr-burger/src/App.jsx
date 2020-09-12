369a693994374a26e4451f070df09d0334a28d60
DaryaYa <lyalyaka@mail.ru>
2020-09-12 17:50:56 +0300

    add: google auth

--- mr-burger/src/App.jsx
+++ mr-burger/src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from 'react'
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { NavBar } from './Components/NavBar/NavBar';
 import { Menu } from './Components/Menu/Menu';
 import { GlobalStyle } from './Components/Style/GlobalStyle';
@@ -6,17 +8,31 @@ import { ModalItem } from './Components/Modal/Modal';
 import { Order } from './Components/Order/Order';
 import { useOpenItem } from "./Components/Hooks/useOpenItem";
 import { useOrders } from "./Components/Hooks/useOrders";
+import { useAuth } from "./Components/Hooks/useAuth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgAWNnMD1FkH_bo2Z029ZS4AdqrS24uXU",
+  authDomain: "mrburger-f3c56.firebaseapp.com",
+  databaseURL: "https://mrburger-f3c56.firebaseio.com",
+  projectId: "mrburger-f3c56",
+  storageBucket: "mrburger-f3c56.appspot.com",
+  messagingSenderId: "147031215131",
+  appId: "1:147031215131:web:013dea3752595b378efcbe",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 
 function App() {
 
+  const auth = useAuth(firebase.auth);
   const openItem = useOpenItem();
   const orders = useOrders();
 
   return (
     <>
       <GlobalStyle />
-      <NavBar />
+      <NavBar {...auth} />
       <Order {...orders} {...openItem} />
       <Menu {...openItem} />
       {openItem.openItem && <ModalItem {...openItem} {...orders} />}
--- mr-burger/src/Components/NavBar/NavBar.jsx
+++ mr-burger/src/Components/NavBar/NavBar.jsx
@@ -39,18 +39,41 @@ const Login = styled.button`
   font-size: 16px;
 `;
 
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
 
+const LogOut = styled.span`
+  font-size: 40px;
+  font-weight: 700px;
+  font-family: "Pacifico", cursive;
+  cursor: pointer;
+  margin: 0 20px;
+`;
 
-export const NavBar = () => (
+export const NavBar = ({ authentication, logIn, logOut }) => (
   <NavBarStyled>
     <Logo>
       <ImgLogo src={logoImg} alt="logo" />
       <H1>Mr Burger</H1>
     </Logo>
-    <Login>
-      <img src={signButton} alt='log in' />
-      <p>Log In</p>
-    </Login>
-    
+    {authentication ? (
+      <User>
+        <figure>
+          <img src={signButton} alt="log out" />
+          <figcaption>{authentication.displayName}</figcaption>
+        </figure>
+        <LogOut title="Log Out" onClick={logOut}>
+          X
+        </LogOut>
+      </User>
+    ) : (
+      <Login onClick={logIn}>
+        <img src={signButton} alt="log in" />
+        <p>Log In</p>
+      </Login>
+    )}
   </NavBarStyled>
 );
